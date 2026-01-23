d3e02bd6f5a2beeed53443e44202eda7f3e53f8e
Tanishka Shishodia <TanishkaS@sdgc.com>
2026-01-23 19:39:48 +0530

    trained with new dataset for good accuracy

--- app.py
+++ app.py
@@ -6,17 +6,30 @@ from huggingface_hub import hf_hub_download
 import cv2
 from xai_occlusion import occlusion_xai
 
-# ---------------- Page Config ----------------
+try:
+    icon = Image.open("logo.png")
+except:
+    icon = "🧠"
+
 st.set_page_config(
     page_title="AI Fake Scene Classifier",
-    page_icon="🧠",
-    layout="centered"
+    page_icon= icon,
+    layout="centered",
+)
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #B5F0BD;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
 
 st.title("🧠 AI Fake Scene Image Classifier")
 st.write("Upload an image to check whether it is **Fake** or **Real**")
 
-# ---------------- Load Model ----------------
 @st.cache_resource
 def load_model():
     model_path = hf_hub_download(
@@ -27,28 +40,24 @@ def load_model():
 
 model = load_model()
 
-# ---------------- File Upload ----------------
 uploaded_file = st.file_uploader(
     "Upload an image",
     type=["jpg", "jpeg", "png"]
 )
 
-# ---------------- Main Logic ----------------
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # ---------- Preprocessing ----------
     image_resized = image.resize((224, 224))
     img_xai = np.array(image_resized) / 255.0      # (224,224,3)
     img_array = np.expand_dims(img_xai, axis=0)    # (1,224,224,3)
 
-    # ---------- Prediction ----------
     pred = model.predict(img_array)[0][0]
 
     st.subheader("Prediction Result")
 
-    if pred >= 0.5:
+    if pred >=0.68:
         predicted_label = "Real"
         predicted_conf = pred
         st.success(f"✅ Real Image\nConfidence: {predicted_conf:.2f}")
@@ -57,7 +66,6 @@ if uploaded_file is not None:
         predicted_conf = 1 - pred
         st.error(f"❌ Fake Image\nConfidence: {predicted_conf:.2f}")
 
-    # ---------- XAI Toggle ----------
     explain = st.checkbox("🔍 Explain Prediction (Occlusion-based XAI)")
 
     if explain:
